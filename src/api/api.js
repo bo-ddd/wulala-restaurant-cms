800b4ae3378d11129a434fd4c42fd622ebc0a996
sw1184499306 <1184499306@qq.com>
2022-10-20 16:51:08 +0800

    更新增加菜品1

--- src/api/api.js
+++ src/api/api.js
@@ -6,13 +6,6 @@ const instance = axios.create({
   headers: { 'Content-Type': 'application/json' }, //在发送服务端之前，前端设置请求头信息；
 });
 
-const foodadd = axios.create({
-  baseURL: baseUrl,
-  timeout: 1000, //如果接口一秒都没有返回结果，则axios会自动帮我们做一个失败(reject)的处理
-  headers: { 'Content-Type': 'multipart/form-data' }, //在发送服务端之前，前端设置请求头信息；
-});
-
-
 //api 在项目中，通常情况指的是 服务端的一个接口方法；
 //一个方法只实现一个功能；所以这个方法就只能是单纯的去调用服务端的接口；
 //这样房子的目的就只是为了可读性，和可维护性；
@@ -43,5 +36,5 @@ export const deleteFood = function (options = {}) {
 }
 
 export const foodAdd = function (options = {}) {
-  return foodadd.post('/food/add', options)
+  return instance.post('/food/add', options)
 }
