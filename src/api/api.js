d7be76eb8cc7a345338f312bb13f1437451c9eae
lwb <1375229126@qq.com>
2022-10-22 15:43:31 +0800

    修改了拦截器的bug

--- src/api/api.js
+++ src/api/api.js
@@ -2,26 +2,32 @@ import axios from "axios";
 let baseUrl = '/api';
 const instance = axios.create({
   baseURL: baseUrl,
-  timeout: 1000, //如果接口一秒都没有返回结果，则axios会自动帮我们做一个失败(reject)的处理
-  headers: { 
-    'Content-Type': 'application/json',
-    authorization: sessionStorage.getItem('token'),
-}, //在发送服务端之前，前端设置请求头信息；
+  timeout: 5000, //如果接口一秒都没有返回结果，则axios会自动帮我们做一个失败(reject)的处理
+  headers: { 'Content-Type': 'application/json',}, //在发送服务端之前，前端设置请求头信息；
   
 });
 
 // 添加请求拦截器
 instance.interceptors.request.use(function (config){
-  config.headers.authorization = sessionStorage.getItem('token');
+  config.headers.token = sessionStorage.getItem('token');
   return config;
 },function(error){
   return Promise.reject(error);
 })
 
+const getPostConfig = function(){
+  return{
+    headers:{
+      contentType: "application/json",
+      token: sessionStorage.getItem('token'),
+    }
+  }
+}
+
 //添加响应拦截器
 instance.interceptors.response.use(function (response){
   if(response.data.status == 401){
-    window.location.href = '/home'
+    window.location.href = '/'
   }
   return response;
 },function(error){
@@ -67,6 +73,6 @@ export const foodAdd = function (options = {}) {
  */
 
 export const userInfoApi = function (payload ={}){
-  return instance.post('/user/info',payload,instance)
+  return instance.post('/user/info',payload,getPostConfig())
 }
 
